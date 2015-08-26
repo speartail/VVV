05c89cc9cfea49c167e7f5eb0e6f18a3b728f456
Peter Hoeg <peter@speartail.com>
2015-08-26 15:29:30 +0800

    create databases as well

--- www/speartail/update_custom_sites.sh
+++ www/speartail/update_custom_sites.sh
@@ -5,40 +5,59 @@ if [[ $(hostname) != "vvv" ]] ; then
   exit 1
 fi
 
-TARGET=/etc/nginx/custom-sites/speartail.conf
+SQLINIT=/srv/database/init-custom.sql
+NGINX=/etc/nginx/custom-sites/speartail.conf
 
 host=$(mktemp)
 conf=$(mktemp)
+sql=$(mktemp)
 
 for site in ../site-* ; do
   site=$(basename $site)
   name=$(echo $site | cut -f2 -d '-')
+  fqdn="${name}.wordpress.dev"
 
   echo "Processing: ${name}"
 
   # hosts
-  echo ${name}.wordpress.dev >> $host
+  echo ${fqdn} >> $host
+
+  # sql
+  cat <<_EOF_ >> $sql
+CREATE DATABASE IF NOT EXISTS \`${name}\`;
+GRANT ALL PRIVILEGES ON \`${name}\`.* TO 'wp'@'localhost' IDENTIFIED BY 'wp';
+GRANT ALL PRIVILEGES ON \`${name}\`.* TO 'external'@'%' IDENTIFIED BY 'external';
+
+_EOF_
 
   # nginx configuration
   cat <<_EOF_ >> $conf
 ################################################################
 # $name
 #
-# Host: http://${name}.wordpress.dev
+# Host: http://${fqdn}
 # Path: /srv/www/${site}
 server {
     listen       80;
     listen       443 ssl;
-    server_name  ${name}.wordpress.dev *.${name}.wordpress.dev ~^${name}\.wordpress\.\d+\.\d+\.\d+\.\d+\.xip\.io$;
+    server_name  ${fqdn} *.${fqdn} ~^${name}\.wordpress\.\d+\.\d+\.\d+\.\d+\.xip\.io$;
     root         /srv/www/${site};
     include      /etc/nginx/nginx-wp-common.conf;
 }
+
 _EOF_
+
 done
 
+# hosts
 mv $host vvv-hosts
-sudo mv $conf $TARGET
-sudo chown root:root $TARGET
-sudo chmod 644 $TARGET
 
+# sql
+echo 'FLUSH PRIVILEGES;' >> $sql
+sudo mv $sql $SQLINIT
+
+# nginx
+sudo mv $conf $NGINX
+sudo chown root:root $NGINX
+sudo chmod 644 $NGINX
 sudo service nginx restart
