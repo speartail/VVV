a67a8e0be34e6b96e2cd6bc8ad9ea5c6d352a4c5
Peter Hoeg <peter@hoeg.com>
2018-04-04 08:42:37 +0800

    use shell.nix

---
+++ shell.nix
@@ -0,0 +1,22 @@
+with import <nixpkgs> {};
+
+let
+  ruby = pkgs.ruby_2_4;
+  bundler = pkgs.bundler.override { inherit ruby; };
+
+in stdenv.mkDerivation rec {
+  name = "env";
+  buildInputs = [
+    libffi
+    # phantomjs2
+    # readline
+    # sqlite # needed by mailcatcher
+    # zlib
+  ];
+
+  nativeBuildInputs = [
+    bundix
+    vagrant
+    wp-cli
+  ];
+}
