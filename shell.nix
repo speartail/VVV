aec2d2e48e7c1ed42a92b49dac9b36af3113608b
Peter Hoeg <peter@hoeg.com>
2019-07-15 15:59:53 +0800

    fix up shell.nix

--- shell.nix
+++ shell.nix
@@ -2,6 +2,10 @@ let
   pkgs = import <nixpkgs> {};
 
 in pkgs.mkShell rec {
+
+  SSL_CERT_FILE = "/etc/ssl/certs/ca-bundle.crt";
+  NIX_SSL_CERT_FILE = SSL_CERT_FILE;
+
   buildInputs = with pkgs; [
     libffi
     libsodium
@@ -10,6 +14,7 @@ in pkgs.mkShell rec {
 
     bundix
     bundler
+    mysql.client # needed for mysqldump which wp-cli needs
     phpPackages.composer
     vagrant
     wp-cli
