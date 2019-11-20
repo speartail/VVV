44ea5583597afd7070b3582b1dcf4892e574d9a4
Peter Hoeg <peter@hoeg.com>
2019-11-20 17:04:20 +0800

    nix

---
+++ shell.nix
@@ -0,0 +1,22 @@
+let
+  pkgs = import <nixpkgs> {};
+
+in pkgs.mkShell rec {
+
+  SSL_CERT_FILE = "/etc/ssl/certs/ca-bundle.crt";
+  NIX_SSL_CERT_FILE = SSL_CERT_FILE;
+
+  buildInputs = with pkgs; [
+    libffi
+    libsodium
+    libxml2
+    zlib
+
+    bundix
+    bundler bundler.ruby
+    mysql.client # needed for mysqldump which wp-cli needs
+    phpPackages.composer
+    vagrant
+    wp-cli
+  ];
+}
